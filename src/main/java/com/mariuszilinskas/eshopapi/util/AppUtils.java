47438e8af5909a626fed5d22011f105dbfa2596f
Marius Zilinskas <mariuzmail@gmail.com>
2024-09-23 21:32:03 +0100

    set up AppUtils class

---
+++ src/main/java/com/mariuszilinskas/eshopapi/util/AppUtils.java
@@ -0,0 +1,11 @@
+package com.mariuszilinskas.eshopapi.util;
+
+public abstract class AppUtils {
+
+    private AppUtils() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static final String TIMESTAMP_FORMAT = "yyyy-MM-dd hh:mm:ss";
+
+}
