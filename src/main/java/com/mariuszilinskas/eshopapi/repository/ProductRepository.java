d4c34d75919b5858af766807d5dd8ce860ed5f67
Marius Zilinskas <mariuzmail@gmail.com>
2024-09-23 20:35:26 +0100

    creates ProductRepository interface

---
+++ src/main/java/com/mariuszilinskas/eshopapi/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.mariuszilinskas.eshopapi.repository;
+
+import com.mariuszilinskas.eshopapi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
