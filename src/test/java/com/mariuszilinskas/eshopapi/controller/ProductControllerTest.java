4932fd83be4ff223d4f64a0c87b450904f619db8
Marius Zilinskas <mariuzmail@gmail.com>
2024-09-24 00:36:27 +0100

    adds tests for product controlller

--- src/test/java/com/mariuszilinskas/eshopapi/controller/ProductControllerTest.java
+++ src/test/java/com/mariuszilinskas/eshopapi/controller/ProductControllerTest.java
@@ -1,43 +1,48 @@
 package com.mariuszilinskas.eshopapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mariuszilinskas.eshopapi.EshopApiApplication;
 import com.mariuszilinskas.eshopapi.dto.ProductRequest;
 import com.mariuszilinskas.eshopapi.dto.ProductResponse;
 import com.mariuszilinskas.eshopapi.enums.Label;
-import com.mariuszilinskas.eshopapi.exception.EntityExistsException;
-import com.mariuszilinskas.eshopapi.exception.ResourceNotFoundException;
 import com.mariuszilinskas.eshopapi.model.Product;
-import com.mariuszilinskas.eshopapi.repository.ProductRepository;
-import com.mariuszilinskas.eshopapi.service.ProductServiceImpl;
+import com.mariuszilinskas.eshopapi.service.ProductService;
 import com.mariuszilinskas.eshopapi.util.AppUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = EshopApiApplication.class)
+@AutoConfigureMockMvc
 public class ProductControllerTest {
 
-    @Mock
-    private ProductRepository productRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @InjectMocks
-    private ProductServiceImpl productService;
+    @MockBean
+    private ProductService productService;
 
     private final int productId = 123;
     private final Product product = new Product();
     private final Product product2 = new Product();
     private ProductRequest productRequest;
+    private ProductResponse productResponse;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     // ------------------------------------
 
@@ -60,104 +65,98 @@ public class ProductControllerTest {
                 product.getPrice(),
                 AppUtils.convertEnumsToStrings(product.getLabels())
         );
+
+        productResponse = new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getPrice(),
+                AppUtils.convertToDate(product.getAddedAt()),
+                AppUtils.convertEnumsToStrings(product.getLabels())
+        );
     }
 
     // ------------------------------------
 
     @Test
-    void testCreateProduct_Success() {
+    public void testCreateProduct() throws Exception {
         // Arrange
-        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+        when(productService.createProduct(any(ProductRequest.class))).thenReturn(productResponse);
 
-        when(productRepository.existsByName(productRequest.name())).thenReturn(false);
-        when(productRepository.save(captor.capture())).thenReturn(product);
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.product_id").value(product.getId()))
+                .andExpect(jsonPath("$.name").value(product.getName()))
+                .andExpect(jsonPath("$.price").value(product.getPrice()))
+                .andExpect(jsonPath("$.labels[0]").value("drink"))
+                .andExpect(jsonPath("$.labels[1]").value("food"));
 
-        // Act
-        ProductResponse response = productService.createProduct(productRequest);
-
-        // Assert
-        assertNotNull(response);
-        assertEquals(product.getId(), response.product_id());
-        assertEquals(product.getName(), response.name());
-        assertEquals(product.getPrice(), response.price());
-        assertEquals(AppUtils.convertEnumsToStrings(product.getLabels()), response.labels());
-
-        verify(productRepository, times(1)).existsByName(productRequest.name());
-        verify(productRepository, times(1)).save(captor.capture());
-
-        Product savedProduct = captor.getValue();
-        assertEquals(productRequest.name(), savedProduct.getName());
-        assertEquals(productRequest.price(), savedProduct.getPrice());
-        assertEquals(productRequest.labels(), AppUtils.convertEnumsToStrings(savedProduct.getLabels()));
-    }
-
-    @Test
-    void testCreateProduct_SameProductNameAlreadyExists() {
-        // Arrange
-        when(productRepository.existsByName(productRequest.name())).thenReturn(true);
-
-        //Act & Assert
-        assertThrows(EntityExistsException.class, () -> productService.createProduct(productRequest));
-
-        // Assert
-        verify(productRepository, times(1)).existsByName(productRequest.name());
-        verify(productRepository, never()).save(any(Product.class));
+        verify(productService, times(1)).createProduct(any(ProductRequest.class));
     }
 
     // ------------------------------------
 
     @Test
-    void testGetAllProducts() {
+    public void testGetAllProducts() throws Exception {
         // Arrange
-        List<Product> products = List.of(product, product2);
+        List<ProductResponse> productList = List.of(
+                productResponse,
+                new ProductResponse(
+                        product2.getId(),
+                        product2.getName(),
+                        product2.getPrice(),
+                        AppUtils.convertToDate(product2.getAddedAt()),
+                        AppUtils.convertEnumsToStrings(product2.getLabels())
+                )
+        );
 
-        when(productRepository.findAll()).thenReturn(products);
+        when(productService.getAllProducts()).thenReturn(productList);
 
-        // Act
-        List<ProductResponse> response = productService.getAllProducts();
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].product_id").value(product.getId()))
+                .andExpect(jsonPath("$[0].name").value(product.getName()))
+                .andExpect(jsonPath("$[1].product_id").value(product2.getId()))
+                .andExpect(jsonPath("$[1].name").value(product2.getName()));
 
-        // Assert
-        assertNotNull(response);
-        assertEquals(products.size(), response.size());
-        assertEquals(product.getId(), response.get(0).product_id());
-        assertEquals(product.getName(), response.get(0).name());
-        assertEquals(product2.getId(), response.get(1).product_id());
-        assertEquals(product2.getName(), response.get(1).name());
-
-        verify(productRepository, times(1)).findAll();
+        verify(productService, times(1)).getAllProducts();
     }
 
     // ------------------------------------
 
     @Test
-    void testGetProduct_Success() {
+    public void testGetProduct() throws Exception {
         // Arrange
-        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(productService.getProduct(productId)).thenReturn(productResponse);
 
-        // Act
-        ProductResponse response = productService.getProduct(productId);
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/" + productId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.product_id").value(product.getId()))
+                .andExpect(jsonPath("$.name").value(product.getName()))
+                .andExpect(jsonPath("$.price").value(product.getPrice()))
+                .andExpect(jsonPath("$.labels[0]").value("drink"))
+                .andExpect(jsonPath("$.labels[1]").value("food"));
 
-        // Assert
-        assertNotNull(response);
-        assertEquals(product.getId(), response.product_id());
-        assertEquals(product.getName(), response.name());
-        assertEquals(product.getPrice(), response.price());
-        assertEquals(AppUtils.convertEnumsToStrings(product.getLabels()), response.labels());
-
-        verify(productRepository, times(1)).findById(productId);
-    }
-
-    @Test
-    void testGetProduct_NonExistentProductId() {
-        // Arrange
-        int nonExistentId = 444;
-        when(productRepository.findById(nonExistentId)).thenReturn(Optional.empty());
-
-        // Assert & Act
-        assertThrows(ResourceNotFoundException.class, () -> productService.getProduct(nonExistentId));
-
-        verify(productRepository, times(1)).findById(nonExistentId);
+        verify(productService, times(1)).getProduct(productId);
     }
 
     // ------------------------------------
+
+    @Test
+    public void testDeleteProduct() throws Exception {
+        // Arrange
+        doNothing().when(productService).deleteProduct(productId);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.delete("/products/" + productId))
+                .andExpect(status().isNoContent());
+
+        verify(productService, times(1)).deleteProduct(productId);
+    }
+
 }
