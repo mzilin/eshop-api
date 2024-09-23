605d712736072f37812bab38fe440b26023e4d75
Marius Zilinskas <mariuzmail@gmail.com>
2024-09-23 23:08:25 +0100

    adds tests for create product method

---
+++ src/test/java/com/mariuszilinskas/eshopapi/service/ProductServiceImplTest.java
@@ -0,0 +1,106 @@
+package com.mariuszilinskas.eshopapi.service;
+
+import com.mariuszilinskas.eshopapi.dto.ProductRequest;
+import com.mariuszilinskas.eshopapi.dto.ProductResponse;
+import com.mariuszilinskas.eshopapi.enums.Label;
+import com.mariuszilinskas.eshopapi.exception.EntityExistsException;
+import com.mariuszilinskas.eshopapi.model.Product;
+import com.mariuszilinskas.eshopapi.repository.ProductRepository;
+import com.mariuszilinskas.eshopapi.util.AppUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    private final int productId = 123;
+    private final Product product = new Product();
+    private final Product product2 = new Product();
+    private ProductRequest productRequest;
+
+    // ------------------------------------
+
+    @BeforeEach
+    void setUp() {
+        product.setId(productId);
+        product.setName("Fancy IPA Beer");
+        product.setPrice(new BigDecimal("5.99"));
+        product.setAddedAt(ZonedDateTime.now());
+        product.setLabels(List.of(Label.DRINK, Label.FOOD));
+
+        product2.setId(124);
+        product2.setName("Delicious Cake");
+        product2.setPrice(new BigDecimal("10.11"));
+        product2.setAddedAt(ZonedDateTime.now());
+        product2.setLabels(List.of(Label.FOOD));
+
+        productRequest = new ProductRequest(
+                product.getName(),
+                product.getPrice(),
+                AppUtils.convertEnumsToStrings(product.getLabels())
+        );
+    }
+
+    // ------------------------------------
+
+    @Test
+    void testCreateProduct_Success() {
+        // Arrange
+        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+
+        when(productRepository.existsByName(productRequest.name())).thenReturn(false);
+        when(productRepository.save(captor.capture())).thenReturn(product);
+
+        // Act
+        ProductResponse response = productService.createProduct(productRequest);
+
+        // Assert
+        assertNotNull(response);
+        assertEquals(product.getId(), response.product_id());
+        assertEquals(product.getName(), response.name());
+        assertEquals(product.getPrice(), response.price());
+        assertEquals(AppUtils.convertEnumsToStrings(product.getLabels()), response.labels());
+
+        verify(productRepository, times(1)).existsByName(productRequest.name());
+        verify(productRepository, times(1)).save(captor.capture());
+
+        Product savedProduct = captor.getValue();
+        assertEquals(productRequest.name(), savedProduct.getName());
+        assertEquals(productRequest.price(), savedProduct.getPrice());
+        assertEquals(productRequest.labels(), AppUtils.convertEnumsToStrings(savedProduct.getLabels()));
+    }
+
+    @Test
+    void testCreateProduct_SameProductNameAlreadyExists() {
+        // Arrange
+        when(productRepository.existsByName(productRequest.name())).thenReturn(true);
+
+        //Act & Assert
+        assertThrows(EntityExistsException.class, () -> productService.createProduct(productRequest));
+
+        // Assert
+        verify(productRepository, times(1)).existsByName(productRequest.name());
+        verify(productRepository, never()).save(any(Product.class));
+    }
+
+    // ------------------------------------
+
+}
