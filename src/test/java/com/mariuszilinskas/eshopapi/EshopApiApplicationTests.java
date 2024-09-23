935ae1a95056adfe58b28b6511fe30c9fe20f961
Marius Zilinskas <mariuzmail@gmail.com>
2024-09-23 18:10:04 +0100

    sets up api app skeleton

---
+++ src/test/java/com/mariuszilinskas/eshopapi/EshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariuszilinskas.eshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
