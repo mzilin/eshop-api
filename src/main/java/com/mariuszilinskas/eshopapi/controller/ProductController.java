2ce63b68ca120147d3b987eb6bb8b48c3e43c78c
Marius Zilinskas <mariuzmail@gmail.com>
2024-09-23 23:47:08 +0100

    creates product controller

---
+++ src/main/java/com/mariuszilinskas/eshopapi/controller/ProductController.java
@@ -0,0 +1,45 @@
+package com.mariuszilinskas.eshopapi.controller;
+
+import com.mariuszilinskas.eshopapi.dto.ProductRequest;
+import com.mariuszilinskas.eshopapi.dto.ProductResponse;
+import com.mariuszilinskas.eshopapi.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest request) {
+        ProductResponse response = productService.createProduct(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+        List<ProductResponse> response = productService.getAllProducts();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable int productId) {
+        ProductResponse response = productService.getProduct(productId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable int productId) {
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
